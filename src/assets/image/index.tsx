a5ec04a10e40ea059bf988928bef1a5547bb8157
Arpit Ubhadiya <117648373+ArpitUbhadiya@users.noreply.github.com>
2023-03-21 15:50:08 +0530

    add DashBord UI

--- src/assets/image/index.tsx
+++ src/assets/image/index.tsx
@@ -2,10 +2,16 @@ import Error404 from "./404.svg";
 import logo from './logo.png'
 import searchIcon from './searchIcon.svg'
 import userIcons from './userIcons.png'
+import slide01 from './slide01.jpg'
+import slide02 from './slide02.jpg'
+import slide03 from './slide03.jpg'
 
 export const Image = {
     Error404,
     logo,
     searchIcon,
-    userIcons
+    userIcons,
+    slide01,
+    slide02,
+    slide03,
 };
--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,11 +1,75 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from "swiper/react";
 import Layout from './Layout'
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/pagination";
+import { Autoplay, EffectFade, Pagination } from "swiper";
+import { Image } from '../assets/image';
 
 const Dashboard = () => {
+
+    const backGroundImage = [Image.slide01, Image.slide02, Image.slide03]
+
     return (
         <Layout >
+            <div className='container'>
+                <Swiper
+                    direction={"vertical"}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    autoplay={{
+                        delay: 3000,
+                        disableOnInteraction: false,
+                    }}
+                    modules={[EffectFade, Pagination,]}//Autoplay
+                    navigation={true}
+                    effect="fade"
+                    className="mySwiper"
+                >
+                    {backGroundImage.map(image => (
+                        <SwiperSlide key={image}>
+                            <div style={{
+                                backgroundImage: `url(${image})`,
+                                backgroundRepeat: 'no-repeat',
+                                backgroundPosition: '50% 50%',
+                                backgroundSize: 'cover',
+                                height: "100vh",
+                                width: "100vw",
+                                backgroundColor: 'black'
+                            }}></div>
+                            <div className='sub-container'>
 
+                                <div className="row align-items-center">
+                                    <div className="col-12 text-center text-sm-left">
+                                        <div className="intro_layers_wrapper">
+                                            <div className="intro_layers">
 
+                                                <div className="intro_layer animated fadeInLeft" data-animation="fadeInLeft" data-delay="150" style={{ visibility: "hidden" }}>
+                                                    <h2 className="text-uppercase thin">academic
+                                                        <span className="color-main text-capitalize bold">chess</span>
+                                                    </h2>
+                                                    <p>
+                                                        We are dedicated to broadening and developing chess as art, recreation, and
+                                                        as a significant element of culture in Northern California.
+                                                    </p>
+                                                </div>
+
+                                                <div className="many-buttons intro_layer animated fadeIn" data-animation="fadeIn" style={{ visibility: "hidden" }}>
+                                                    <a href="/" className="btn btn-maincolor medium-btn">start now</a>
+                                                    <a href="/" className="btn btn-outline-darkgrey medium-btn">learn more</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
         </Layout>
     )
 }
